92253876ced7724d14f061084af595bc62d6d2de
JulianaForbici <julianaforbicii@gmail.com>
2025-11-12 07:35:30 -0300

    Concatenação de números

--- src/main/java/exemplos/LeituraTeclado.java
+++ src/main/java/exemplos/LeituraTeclado.java
@@ -8,5 +8,11 @@ public class LeituraTeclado {
         System.out.println("Digite um valor: ");
         int numero01 = teclado.nextInt();
         System.out.println(numero01);
+        System.out.println("Digite uma palavra: ");
+        String mensagem = teclado.next();
+        System.out.println(mensagem);
+        System.out.println("Digite outro número: ");
+        int numero02 = teclado.nextInt();
+        System.out.println("Soma de numeros: " + (numero01 + numero02));
     }
 }
